e83484bddcaac6d50ec6b3a7ae2e0984c8336be2
PristavuDragos <62354493+PristavuDragos@users.noreply.github.com>
2020-05-06 23:30:04 +0300

    Add files via upload

--- Client/GameClient.java
+++ Client/GameClient.java
@@ -11,7 +11,8 @@ import java.util.Scanner;
 public class GameClient {
     private String serverAddress;
     private int PORT;
-
+    private boolean firstMove =false;
+    private boolean playing=false;
     private GameClient() { }
     public GameClient(String address,int port){
         this.serverAddress=address;
@@ -23,7 +24,7 @@ public class GameClient {
         }
     }
 
-    private void runClient() throws IOException {
+    private boolean runClient() throws IOException {
         try (   Socket socket = new Socket(serverAddress, PORT);
                 PrintWriter out =
                         new PrintWriter(socket.getOutputStream(), true);
@@ -34,16 +35,59 @@ public class GameClient {
             String request="";
             Scanner scan= new Scanner(System.in);
             do {
-                System.out.println("Awaiting your command");
-                request=scan.nextLine();
-                out.println(request);
-                String response = in.readLine();
-                if (request.compareTo("exit")!=0)System.out.println(response);
-                else System.out.println("Closing the client.");
+                if (playing){
+                    if (firstMove){
+                        System.out.println("Submit your move");
+                        request= scan.nextLine();
+                        out.println(request);
+                        String response="";
+                        if (request.length() >= 11 && request.substring(0, 11).compareTo("submit move") == 0) {
+                            //response=in.readLine();
+                            char[] buffer = new char[1024];
+                            in.read(buffer);
+                            response=new String(buffer);
+                            System.out.println(response);
+                            if (response.indexOf("won!")!=-1) return true;
+                            System.out.println("Waiting for the other player to move");
+                            buffer = new char[1024];
+                            in.read(buffer);
+                            response=new String(buffer);
+                            System.out.println(response);
+                            if (response.indexOf("won!")!=-1) return true;
+                        } else if (request.compareTo("exit") != 0) System.out.println(response);
+                        else System.out.println("Closing the client.");
+                    } else{
+                        System.out.println("Waiting for the other player to move");
+                        String response="";
+                        char[] buffer = new char[1024];
+                        in.read(buffer);
+                        response=new String(buffer);
+                        System.out.println(response);
+                        if (response.indexOf("won!")!=-1) return true;
+                        System.out.println("Submit your move");
+                        request= scan.nextLine();
+                        out.println(request);
+                        buffer = new char[1024];
+                        in.read(buffer);
+                        response=new String(buffer);
+                        System.out.println(response);
+                        if (response.indexOf("won!")!=-1) return true;
+                    }
+                } else {
+                    System.out.println("Awaiting your command");
+                    request = scan.nextLine();
+                    out.println(request);
+                    if (request.compareTo("create game") == 0) firstMove = true;
+                    String response = in.readLine();
+                    if (response.length() >= 7 && response.substring(0, 7).compareTo("Playing") == 0) playing = true;
+                    if (request.compareTo("exit") != 0) System.out.println(response);
+                    else System.out.println("Closing the client.");
+                }
             }while(request.compareTo("exit")!=0);
         } catch (UnknownHostException e) {
             System.err.println("No server listening... " + e);
         }
+        return false;
     }
 
 }
